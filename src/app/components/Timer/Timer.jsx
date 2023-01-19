a1c8e9eabd3acfbb25ac30dd87753c53552838cc
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-01-19 16:34:57 +0100

    timer

--- src/app/components/Timer/Timer.jsx
+++ src/app/components/Timer/Timer.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from 'react';
 import './timer.css';
 
-const Timer = () => {
+const Timer = ({session}) => {
+    const [time, setTime] = useState(null)
+
+    const parseTime = (result) => {
+        if(result < 10) {
+            result = '0' + result
+        }
+        return result
+    }
+
+    const getSessionTime = (data) => {
+        const sessionTime = data.map((session) => {
+            if(session.fields['Status'] === "open"){
+                return session.fields['Heure fin']
+            }
+        })
+        const sessionTimeLeft = Date.parse(sessionTime[0]) - Date.now()
+
+        if(sessionTimeLeft < 0) {
+            return "00:00:00"
+        }
+
+        const sessionTimeHours = Math.floor((sessionTimeLeft / (1000 * 60 * 60)))
+        const sessionTimeMinutes = Math.floor((sessionTimeLeft / 1000 / 60) % 60)
+        const sessionTimeSeconds = Math.floor((sessionTimeLeft / 1000) % 60)
+
+        const sessionTimeHoursParse = parseTime(sessionTimeHours)
+        const sessionTimeMinutesParse = parseTime(sessionTimeMinutes)
+        const sessionTimeSecondsParse = parseTime(sessionTimeSeconds)
+
+        return `${sessionTimeHoursParse}:${sessionTimeMinutesParse}:${sessionTimeSecondsParse}`
+    }
+    useEffect(() => {
+        setTime(getSessionTime(session))
+    }, [session])
     return (
         <div className="timer">
             {
@@ -8,7 +43,6 @@ const Timer = () => {
                 ? <p className="time">00:00:00</p> 
                 : <p className="time">{time}</p>
             }
-
         </div>
     );
 }
